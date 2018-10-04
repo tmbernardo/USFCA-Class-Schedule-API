7d841f9c3cba23e12d92b70535ed445b5b052f84
Matthew Bernardo <tmbernardo@dons.usfca.edu>
2018-10-03 21:52:55 -0700

    changed schema and fixed courses post

--- class_registry/__init__.py
+++ class_registry/__init__.py
@@ -2,6 +2,7 @@ from class_registry import dbhandler as db
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
+import json
 import markdown
 import os
 import sys
@@ -16,6 +17,7 @@ def _run_on_start():
     for _ in range(60):
         try:
             db.create_tables()
+            print('Database schema created', file=sys.stdout)
             return
         except Exception as e:
             time.sleep(1)
@@ -37,6 +39,25 @@ def index():
 
 class CourseList(Resource):
     def get(self):
-        return {'message': 'Success' , 'data': db.get_classes()}, 200
+        return {'message': 'Success' , 'data': db.get_courses()}, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('id', required = True)
+        parser.add_argument('title', required = True)
+        parser.add_argument('course_num', required = True)
+        parser.add_argument('section_num', required = True)
+        parser.add_argument('capacity', required = True)
+        parser.add_argument('actual', required = True)
+        parser.add_argument('course_url', required = True)
+        parser.add_argument('instructor', required = True)
+        parser.add_argument('dept', required = True)
+
+        args = parser.parse_args()
+
+        db.insert_course(args)
+        
+        return {'message': 'Course registered', 'data': args}, 201
 
 api.add_resource(CourseList, '/courses')
--- class_registry/dbhandler.py
+++ class_registry/dbhandler.py
@@ -13,33 +13,45 @@ engine = sql.create_engine('mysql+mysqlconnector://root:password@db:3306/usf')
 class Instructors(Base):
     __tablename__ = "instructors"
     id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False, unique=True)
-    dept = Column(Integer, nullable=False)
+    name = Column(String(64), nullable=False, unique=True)
 
 # Departments
 class Departments(Base):
     __tablename__ = "departments"
     id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False, unique=True)
+    name = Column(String(64), nullable=False, unique=True)
 
 # Students/Users
 class Students(Base):
     __tablename__ = "students"
     id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False, unique=True)
+    name = Column(String(64), nullable=False, unique=True)
 
 # Courses
 class Courses(Base):
     __tablename__ = "courses"
     id = Column(Integer, primary_key=True)
-    title = Column(String(128), nullable=False)
+    title = Column(String(64), nullable=False)
     course_num = Column(Integer, nullable=False)
     section_num = Column(Integer, nullable=False)
     capacity = Column(Integer, nullable=False)
     actual = Column(Integer, nullable=False)
-    course_url = Column(String(512), nullable=False)
+    course_url = Column(String(256), nullable=False)
     instructor = Column(Integer, ForeignKey('instructors.id'))
     dept = Column(Integer, ForeignKey('departments.id'))
+
+    def serialize(self):
+        return {
+                'id' : self.id,
+                'title' : self.title,
+                'course_num' : self.course_num,
+                'section_num' : self.section_num,
+                'capacity' : self.capacity,
+                'actual' : self.actual,
+                'course_url' : self.course_url,
+                'instructor' : self.instructor,
+                'dept' : self.dept
+                }
 
 def start_sess():
     Session = sessionmaker(bind=engine, autocommit=False)
@@ -50,11 +62,47 @@ def create_tables(sess=start_sess()):
     sess.commit()
     sess.close()
 
-def get_classes(sess=start_sess()):
+def instructor_exists(instructor, sess=start_sess()):
+    return sess.query(Instructors).filter(Instructors.name==instructor).scalar()
+
+def dept_exists(dept, sess=start_sess()):
+    return sess.query(Departments).filter(Departments.name==dept).scalar()
+
+def get_courses(sess=start_sess()):
     query = sess.query(Courses).all()
-    dump =  json.dumps([ row._asdict() for row in query ])
+    dump =  [ row.serialize() for row in query ]
     sess.close()
     return dump
+
+def insert_course(args, sess=start_sess()):
+    course = Courses(
+            id = args['id'],
+            title = args['title'],
+            course_num = args['course_num'],
+            section_num = args['section_num'],
+            capacity = args['capacity'],
+            actual = args['actual'],
+            course_url = args['course_url'],
+            )
+    inst = instructor_exists(args['instructor'])
+    dep = dept_exists(args['dept'])
+
+    if not inst:
+        inst = Instructors(name=args['instructor'])
+        sess.add(inst)
+        sess.commit()
+
+    if not dep:
+        dep = Departments(name=args['dept'])
+        sess.add(dep)
+        sess.commit()
+
+    course.instructor = inst.id
+    course.dept = dep.id
+    sess.add(course)
+    sess.commit()
+    sess.close()
+    return True
 
 def insert_student(value, sess=start_sess()):
     user = Students(name=value)
